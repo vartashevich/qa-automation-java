e2497c4ed73159aca7d19e113d226588d43fc09f
v.artashevich <ESwawl4ap>
2022-05-05 10:25:02 +0300

    fix: Убрал лишние комментарии

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -14,21 +14,11 @@ class Application {
      * @param args аргументы командной строки передаваемые строками
      */
     public static void main(String[] args) {
-      /*  for (int i = 0; i < 6; i++) {
-            MessageService.logMessage(Severity.MAJOR, "My major message! ");
-        }
-        MessageService.logMessage(null, MessageOrder.DESC, "111", "222", "333");
-        MessageService.logMessage(Severity.REGULAR, MessageOrder.ASC, "aaa", "bbb", "ccc");
-        MessageService.logMessage(Severity.REGULAR, MessageOrder.ASC, Doubling.DISTINCT, "!!!", "@@@", "###", "###", "@@@", "!!!");
-        MessageService.logMessage(Severity.REGULAR, MessageOrder.ASC, (Doubling) null, "!!!", "@@@", "###", "###", "@@@", "!!!");
-        Message message1= new Message("Hello!", Severity.REGULAR);
-
-       */
         Message m1 = new Message("Hello!", Severity.REGULAR);
         MessageService.logMessage(m1);
         Message m2 = new Message("Hello2!", Severity.MAJOR);
-        MessageService.logMessage(MessageOrder.ASC,m2);
-       MessageService.logMessage(MessageOrder.ASC, m1,m2);
-       MessageService.logMessage(MessageOrder.DESC,Doubling.DOUBLES,m1,m2);
+        MessageService.logMessage(MessageOrder.ASC, m2);
+        MessageService.logMessage(MessageOrder.ASC, m1, m2);
+        MessageService.logMessage(MessageOrder.DESC, Doubling.DOUBLES, m1, m2);
     }
 }
